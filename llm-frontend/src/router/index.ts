4c71ff48f0e664ca5b6a31151faedec71ebc6a49
hopner <hopner01@gmail.com>
2025-08-26 16:57:26 +0200

    Remove Explain view and its route from the router

--- llm-frontend/src/router/index.ts
+++ llm-frontend/src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Builder from '../views/Builder.vue'
-import Explain from '../views/Explain.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'Overvie', component: HomeView },
     { path: '/builder', name: 'Builder', component: Builder },
-    { path: '/explain/:strategy', name: 'Explain', component: Explain },
   ]
 })
