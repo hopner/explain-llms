ab57f180800a60a60dc932a285fe972bce2a72e7
hopner <hopner01@gmail.com>
2025-10-07 15:24:26 +0200

    Enhance UserAdmin with list_select_related and delete permission actions

--- django-backend/users/admin.py
+++ django-backend/users/admin.py
@@ -6,3 +6,9 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['guid', 'created_at', 'updated_at']
     readonly_fields = ['guid', 'created_at', 'updated_at']
     search_fields = ['guid']
+    list_select_related = True
+
+    def has_delete_permission(self, request, obj = ...):
+        return True
+    
+    actions = ['delete_selected']
