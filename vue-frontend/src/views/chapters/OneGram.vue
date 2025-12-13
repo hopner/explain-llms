b5ea6719b4806f74731522a5224791bc1c0d2698
hopner <hopner01@gmail.com>
2025-12-13 17:00:42 +0100

    Better filler text in stories

--- vue-frontend/src/views/chapters/OneGram.vue
+++ vue-frontend/src/views/chapters/OneGram.vue
@@ -2,12 +2,12 @@
 import ChapterWrapper from '../../components/ChapterWrapper.vue';
 
 const steps = [
-    { title: "", text: "The AI reads text without understanding the meaning", slide: 0},
-    { title: "", text: "The AI counts how often each word appears", slide: 1},
-    { title: "", text: "Estimate the probability of each word", slide: 2},
-    { title: "", text: "The model select the highest probability word" , slide: 3},
-    { title: "", text: "This quickly becomes repetitive, deterministic, too simple to capture context" , slide: 4},
-    { title: "", text: "frequency matters, but words alone aren’t enough to make language meaningful" , slide: 5}
+    { title: "", text: "Right now, the AI reads text without understanding the meaning behind the words. It simply adds them to its vocabulary without any sense of which words usually appear together.", slide: 0},
+    { title: "", text: "We can make the AI a bit smarter by teaching it to recognize individual words and their frequencies.", slide: 1},
+    { title: "", text: "If we have the frequency of each word, we can estimate the probability of each word appearing next.", slide: 2},
+    { title: "", text: "The model should be more likely to select the word with the highest probability so let us have it do that.", slide: 3},
+    { title: "", text: "This quickly becomes repetitive as it is deterministic. The model is still too simple to capture context of each token." , slide: 4},
+    { title: "", text: "The frequency of each token matters, but words alone aren’t enough to make language any meaningful." , slide: 5}
 ];
 </script>
 
